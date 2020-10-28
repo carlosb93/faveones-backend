d1fb8ff8c6dc65df56da22bb66565a896762cd8e
Fabian Gamboa <fabiangamboa95@gmail.com>
2020-10-28 15:45:52 -0400

    images fixed

--- services/post.service.js
+++ services/post.service.js
@@ -1,7 +1,8 @@
 const db = require('_helpers/db');
 const paginate = require('jw-paginate');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
 const _ = require('lodash');
+const { param } = require('express-validator');
 
 
 
@@ -16,39 +17,22 @@ module.exports = {
     delete: _delete
 };
 
-async function newPost(params,user,req,res) {
-    let data = [];
-    
-    if(!req.files) {
-        params.image = '';
-    }else{
-        _.forEach(_.keysIn(req.files.photos), (key) => {
-            let photo = req.files.photos[key];
-            
-            //move photo to upload directory
-            photo.mv('../uploads/' + user.id +'-'+ user.email +'/publicaciones/image-'+ photo.name);
+async function newPost(params, user, req, res) {
+    // console.log(params) // dev checked
 
-            //push file details
-            data.push({
-                url: '../uploads/' + user.id +'-'+ user.email +'/publicaciones/image-'+ photo.name,
-                name: photo.name,
-                mimetype: photo.mimetype,
-                size: photo.size
-            });
-        });
-    }
-    params.image = JSON.stringify(data);
     params.user_id = user.id;
-        // Guardando posts
-    await db.Post.create(params);
+    params.image = JSON.stringify(params.image)
+    try {
+        await db.Post.create(params);
+    } catch (e) { console.log(e, 'errrrrrroor!!!!!!') }
 }
 
 async function getAll(id) {
     sevenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 7));
     today = new Date(new Date().setDate(new Date().getDate()));
-    
-    posts = await db.Post.findAll({where:{ createdAt:{ [Op.gt]:sevenDaysAgo, [Op.lt]:today }}, order:[['createdAt','DESC']], include: ['profile']});
-    
+
+    posts = await db.Post.findAll({ where: { createdAt: { [Op.gt]: sevenDaysAgo, [Op.lt]: today } }, order: [['createdAt', 'DESC']], include: ['profile'] });
+
     const page = parseInt(id) || 1;
 
     // get pager object for specified page
@@ -57,27 +41,31 @@ async function getAll(id) {
     // get page of items from items array
     const pageOfPosts = posts.slice(pager.startIndex, pager.endIndex + 1);
 
-    posts = {pager, pageOfPosts};
+    posts = { pager, pageOfPosts };
 
     return posts;
 }
 // http://localhost:4000/api/post/mine?search=cuba&page=1
-async function getAllMine(user,req) {
-    if(typeof req.query.search != 'undefined'){
+async function getAllMine(user, req) {
+    if (typeof req.query.search != 'undefined') {
 
-        posts = await db.Post.findAll({where:{ user_id: user.id,
-            [Op.or]: [
-            {title:{[Op.like]: '%' + req.query.search + '%'}}, 
-            {content:{[Op.like]: '%' + req.query.search + '%'}}
-        ]}, include: ['profile']});
+        posts = await db.Post.findAll({
+            where: {
+                user_id: user.id,
+                [Op.or]: [
+                    { title: { [Op.like]: '%' + req.query.search + '%' } },
+                    { content: { [Op.like]: '%' + req.query.search + '%' } }
+                ]
+            }, include: ['profile']
+        });
     }
-    else{
-        posts = await db.Post.findAll({where:{ user_id: user.id,}, order:[['createdAt','DESC']], include: ['profile']});
+    else {
+        posts = await db.Post.findAll({ where: { user_id: user.id, }, order: [['createdAt', 'DESC']], include: ['profile'] });
     }
 
     // if(typeof req.query.page != 'undefined'){
 
-      page = parseInt(req.query.page) || 1;
+    page = parseInt(req.query.page) || 1;
     // }
 
     // get pager object for specified page
@@ -86,7 +74,7 @@ async function getAllMine(user,req) {
     // get page of items from items array
     const pageOfPosts = posts.slice(pager.startIndex, pager.endIndex + 1);
 
-    posts = {pager, pageOfPosts};
+    posts = { pager, pageOfPosts };
 
     return posts;
 }
@@ -98,18 +86,22 @@ async function searchPosts(req) {
     sevenDaysAgo = new Date(new Date().setDate(new Date().getDate() - 7));
     today = new Date(new Date().setDate(new Date().getDate()));
 
-    if(typeof req.query.search != 'undefined'){
+    if (typeof req.query.search != 'undefined') {
         // query
-        posts = await db.Post.findAll({where:{ createdAt:{ [Op.gt]:sevenDaysAgo, [Op.lt]:today },[Op.or]: [
-            {title:{[Op.like]: '%' + req.query.search + '%'}}, 
-            {content:{[Op.like]: '%' + req.query.search + '%'}}
-        ] }, order:[['createdAt','DESC']], include: ['profile']});
-      }
-      if(typeof req.query.page != 'undefined'){
+        posts = await db.Post.findAll({
+            where: {
+                createdAt: { [Op.gt]: sevenDaysAgo, [Op.lt]: today }, [Op.or]: [
+                    { title: { [Op.like]: '%' + req.query.search + '%' } },
+                    { content: { [Op.like]: '%' + req.query.search + '%' } }
+                ]
+            }, order: [['createdAt', 'DESC']], include: ['profile']
+        });
+    }
+    if (typeof req.query.page != 'undefined') {
 
         pag = parseInt(req.query.page) || 1;
 
-      }
+    }
 
     // get pager object for specified page
     const pager = paginate(posts.length, pag);
@@ -117,7 +109,7 @@ async function searchPosts(req) {
     // get page of items from items array
     const pageOfPosts = posts.slice(pager.startIndex, pager.endIndex + 1);
 
-    posts = {pager, pageOfPosts};
+    posts = { pager, pageOfPosts };
 
     return posts;
 }
@@ -133,26 +125,26 @@ async function getById(id) {
 async function update(id, params) {
     const post = await getPost(id);
 
-    if(!req.files) {
-        
-    }else{
+    if (!req.files) {
+
+    } else {
         let data = [];
         _.forEach(_.keysIn(req.files.photos), (key) => {
             let photo = req.files.photos[key];
-            
+
             //move photo to upload directory
-            photo.mv('../uploads/' + post.user.id +'-'+ post.user.email +'/publicaciones/image-'+ photo.name);
+            photo.mv('../uploads/' + post.user.id + '-' + post.user.email + '/publicaciones/image-' + photo.name);
 
             //push file details
             data.push({
-                url: '../uploads/' + post.user.id +'-'+ post.user.email +'/publicaciones/image-'+ photo.name,
+                url: '../uploads/' + post.user.id + '-' + post.user.email + '/publicaciones/image-' + photo.name,
                 name: photo.name,
                 mimetype: photo.mimetype,
                 size: photo.size
             });
         });
     }
-    
+
     // Validando
     const titleChanged = params.title && post.title !== params.title;
     if (titleChanged && await db.Post.findOne({ where: { title: params.title } })) {
@@ -181,7 +173,7 @@ async function _delete(id) {
 // Funciones de ayuda
 
 async function getPost(id) {
-    const post = await db.Post.findByPk({where:{ id:{id}}, include: ['profile']});
+    const post = await db.Post.findByPk({ where: { id: { id } }, include: ['profile'] });
     if (!post) throw 'Post not found';
     return post;
 }
